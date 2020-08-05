c50da133dc1c7c22ea1d3f309189a168b5e6fa67
OlegMatvienko <mat_ol@mail.ru>
2020-08-05 10:27:02 +0500

    Disable ssl

--- Code/CompetenceManagementSystem/Web/Startup.cs
+++ Code/CompetenceManagementSystem/Web/Startup.cs
@@ -51,7 +51,7 @@ namespace CompetenceManagementSystem.Web
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
